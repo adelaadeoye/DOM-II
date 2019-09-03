25e864c41a215513f2284bef1474b8e314fb5132
adeoye adela <adeoye.adela@gmail.com>
2019-09-04 00:04:16 +0100

    Event 2 and 3 added doubleclick and click event

--- js/index.js
+++ js/index.js
@@ -13,11 +13,27 @@ mouseAction.forEach(item => {
     });
 })
 mouseAction.forEach(item => {
-    item.addEventListener('mouseleave', (e) => {
-        item.style.border = "";
-        item.style.borderRadius = "";
-        // preventDefault();
+        item.addEventListener('mouseleave', (e) => {
+            item.style.border = "";
+            item.style.borderRadius = "";
+            // preventDefault();
 
 
-    });
+        });
+    })
+    //Event 2 and 3: doubleClick single click plus append 
+var button = document.createElement('button'); // Create a <li> node
+var textnode = document.createTextNode(" Click for Fun"); // Create a text node
+button.appendChild(textnode); // Append the text to <li>
+document.querySelector(".intro").appendChild(button);
+
+let doubleClick = document.querySelector('.intro button');
+doubleClick.addEventListener('dblclick', (e) => {
+    document.querySelector('.intro img').src = "img/funbus.gif"
+    button.textContent = "Hahahahahahah"
+})
+let singleClick = document.querySelector('.intro button');
+doubleClick.addEventListener('click', (e) => {
+    document.querySelector('.intro img').src = "img/funbus2.gif"
+    button.textContent = "Double Click for more fun"
 })
