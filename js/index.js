6963b4c8f93589b8a4bc6d1eb3fab079f4957e50
adeoye adela <adeoye.adela@gmail.com>
2019-09-04 00:03:17 +0100

    Event 1: Added a Mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+//Event 1: Mouseover
+let mouseAction = document.querySelectorAll('a');
+mouseAction.forEach(item => {
+    item.addEventListener('mouseover', (e) => {
+        item.style.border = "1px solid black";
+        item.style.borderRadius = "5px";
+        item.style.width = "70px"
+        item.style.textAlign = "center"
+            // preventDefault();
+
+
+    });
+})
+mouseAction.forEach(item => {
+    item.addEventListener('mouseleave', (e) => {
+        item.style.border = "";
+        item.style.borderRadius = "";
+        // preventDefault();
+
+
+    });
+})
